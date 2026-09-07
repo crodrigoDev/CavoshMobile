56d08ad2336e48b96947d6edef60b8ba198d6082
crodrigoDev <212727257+crodrigoDev@users.noreply.github.com>
2026-09-06 22:23:33 -0500

    feat(splash): aÃagregar navegacion hacia login

--- app/src/main/java/com/crodrigo/cavosh/ui/splash/SplashFragment.java
+++ app/src/main/java/com/crodrigo/cavosh/ui/splash/SplashFragment.java
@@ -2,18 +2,48 @@ package com.crodrigo.cavosh.ui.splash;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.crodrigo.cavosh.R;
+import com.crodrigo.cavosh.databinding.FragmentSplashBinding;
 
 public class SplashFragment extends Fragment {
 
-    public SplashFragment() {
-        super(R.layout.fragment_splash);
+    private FragmentSplashBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentSplashBinding.inflate(
+                inflater,
+                container,
+                false
+        );
+        return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding = FragmentSplashBinding.bind(view);
+
+        binding.btnComenzar.setOnClickListener(v ->
+                NavHostFragment.findNavController(this)
+                        .navigate(R.id.action_navigation_splash_to_login)
+        );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
