10d60513c9343cfc7c961f8c6b03b6cd543459c8
crodrigoDev <212727257+crodrigoDev@users.noreply.github.com>
2026-09-08 17:09:06 -0500

    feat(auth): agregar navegacion basica entre Login, Registrar e Inicio

--- app/src/main/java/com/crodrigo/cavosh/ui/auth/LoginFragment.java
+++ app/src/main/java/com/crodrigo/cavosh/ui/auth/LoginFragment.java
@@ -2,18 +2,44 @@ package com.crodrigo.cavosh.ui.auth;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.crodrigo.cavosh.R;
+import com.crodrigo.cavosh.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
+
+    private FragmentLoginBinding binding;
 
     public LoginFragment() {
         super(R.layout.fragment_login);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding = FragmentLoginBinding.bind(view);
+
+        binding.btnTabRegistrar.setOnClickListener(v ->
+                NavHostFragment.findNavController(this)
+                        .navigate(R.id.action_navigation_login_to_navigation_registrar));
+
+        binding.btnLogin.setOnClickListener(v ->
+                NavHostFragment.findNavController(this)
+                        .navigate(R.id.action_navigation_login_to_navigation_inicio));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
